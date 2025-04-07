e9ddbf3ce28114ad88e28d661212ef1b40d87a8b
priyawaw <priyawaw@gmail.com>
2025-04-07 21:35:44 +0530

    next commit

---
+++ dataTypes.ts
@@ -0,0 +1,6 @@
+// Data Type : Define what kind of data a variable is storing
+// Primitive Data Types: Numbers, Strings, Booleans, Null, Undefined, Any , Union 
+// Non-Primitive Data Types: Objects, Arrays
+
+// Type annotation: 
+// Type interferance:
