ddafed80bac5f27fa2fb0bf026b2f2b9bebcee58
priyawaw <priyawaw@gmail.com>
2025-04-14 13:54:21 +0530

    DT practice

---
+++ dataTypePractice.ts
@@ -0,0 +1,41 @@
+// number
+
+var a : number = 25
+var b = 23
+let sum = a + b
+console.log(sum)
+
+// string
+
+let Fname = 'Priya'
+const Lname = " Bhopale"
+console.log(Fname  +  Lname)
+
+// boolean
+
+var boolean = true
+console.log(boolean)
+
+// null
+
+let data : null = null
+console.log(data)
+
+// undefined
+
+let undefine : undefined
+console.log(undefine)
+
+// any
+
+let value : any = 10
+value = 'string'
+value = 20.5
+console.log(value)
+
+// union
+
+let uni : number | string | Boolean = 84
+console.log(uni)
+
+
